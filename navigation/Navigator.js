786846413a5154f793ebfea49b04566b4b868d75
Eveline <eveltum27@gmail.com>
2020-08-07 19:17:50 +0300

    icons added

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,20 +1,34 @@
 import FeedScreen from '../screens/FeedScreen'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ProfileScreen from '../screens/ProfileScreen'
+import React from 'react'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 const Navigator = createMaterialBottomTabNavigator({
 	Feed: {
 		screen: FeedScreen,
 		navigationOptions: {
-			title: 'Новости'
-		}
+			tabBarLabel: "Новости",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon name="home" color={tintColor} size={26} />
+			)
+		},
 	},
 	Profile: {
 		screen: ProfileScreen,
 		navigationOptions: {
-			title: 'Профиль'
+			tabBarLabel: "Профиль",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon name="content-cut" color={tintColor} size={26} />
+			)
 		}
 	}
+}, {
+			// activeTintColor: 'black',
+		inactiveTintColor: '#EFECF4',
+		style: {
+			backgroundColor: 'white',
+		}
 })
 
 export default Navigator
