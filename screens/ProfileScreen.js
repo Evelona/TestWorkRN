d0d9fd676c94f9cde6607d5e7edd7f4028376087
Eveline <eveltum27@gmail.com>
2020-08-09 22:58:25 +0300

    Camera and Gallery added

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,36 +1,59 @@
-// import * as / from 'expo'
-
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
 import { Avatar, Button } from 'react-native-elements';
-import { Dimensions, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { Component, useState } from "react"
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { Component, useRef, useState } from "react"
 
-const option = {
-	title: 'Выберите:',
-	takePhotoButtonTitle: 'Camera',
-	chooseFromLibraryButtonTitle: 'Photo Library',
-
-}
-
-
+import Animated from 'react-native-reanimated'
+import BottomSheet from 'reanimated-bottom-sheet'
 
 const ProfileScreen = () => {
 	const [name, setName] = useState('Eveline')
 	const [editing, setEditing] = useState(false)
 	const [status, setStatus] = useState('start')
-	const [image, setImage] = useState(null)
+	const bs = useRef()
+	const fall = new Animated.Value(1);
+	const [image, setImage] = useState('https://i0.wp.com/stephenafamo.com/blog/wp-content/uploads/2019/10/React-Native-1.png?fit=1200%2C675&ssl=1s')
 	const [description, setDescription] = useState('Очень люблю кошек и собак, верстать и прогать')
-	console.log(image)
 	const cycle = {
 		editing: 'Сохранить',
 		nothing: 'Отменить',
 		start: 'Редактировать'
 	}
+
+	const renderInner = () => (
+		<View style={styles.panelContent}>
+			<View style={{ alignItems: 'center' }}>
+				<Text style={styles.uploadTitle}>Upload Photo</Text>
+			</View>
+
+			<TouchableOpacity style={styles.buttonBS} onPress={selectPhotoFromCamera}>
+				<Text style={styles.panelSubtitle}>Take Photo</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.buttonBS} onPress={selectPhotoFromGallery}>
+				<Text style={styles.panelSubtitle}>Choose From Library</Text>
+			</TouchableOpacity>
+			<TouchableOpacity
+				style={styles.buttonBS}
+				onPress={() => bs.current.snapTo(1)}>
+				<Text style={styles.panelSubtitle}>Cancel</Text>
+			</TouchableOpacity>
+		</View>
+	);
+
+	const renderHeader = () => (
+		<View style={styles.headerBS}>
+			<View style={styles.panel}>
+				<View style={styles.panelHandle} />
+			</View>
+		</View>
+	);
+
+
 	const selectPhotoFromGallery = async () => {
 		await Permissions.askAsync(Permissions.CAMERA_ROLL)
-		const { cancelled, uri}  = await ImagePicker.launchImageLibraryAsync({
+		const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
 			mediaTypes: ImagePicker.MediaTypeOptions.All,
 			allowsEditing: true,
 			aspect: 1,
@@ -38,14 +61,32 @@ const ProfileScreen = () => {
 		});
 		if (!cancelled) {
 			setImage(uri);
+			bs.current.snapTo(1);		}
+	}
+
+	const selectPhotoFromCamera = async () => {
+		await Permissions.askAsync(Permissions.CAMERA)
+		const { cancelled, uri } = await ImagePicker.launchCameraAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: false
+		});
+		if (!cancelled) {
+			setImage(uri);
+			bs.current.snapTo(1)
 		}
 	}
-	// const choosePhoto = () => {
-	// 	alert('click')
-	// }
-
+	
 	return (
 		<View style={styles.container}>
+			<BottomSheet
+				ref={bs}
+				snapPoints={[280, 0]}
+				initialSnap={1}
+				renderContent={renderInner}
+				renderHeader={renderHeader}
+				callbackNode={fall}
+				enabledGestureInteraction={true}
+				/>
 			{!editing ?
 				<View style={styles.header}>
 					<Text style={styles.title}>{name}</Text>
@@ -60,16 +101,19 @@ const ProfileScreen = () => {
 						setStatus('editing')
 					}}
 				/> : null}
-			<Avatar
+			<TouchableOpacity onPress={() => bs.current.snapTo(0)}>
+				<Avatar
 				size="xlarge"
 				rounded
 				title={name[0]}
-				onPress={() => selectPhotoFromGallery()}
+				
 				source={{uri:image}}
 				// source={{ uri: 'https://i0.wp.com/stephenafamo.com/blog/wp-content/uploads/2019/10/React-Native-1.png?fit=1200%2C675&ssl=1' }}
 				avatarStyle = {styles.avatar}
 				activeOpacity={0.7}
 			/>
+			</TouchableOpacity>
+			
 			{!editing ? <Text style={styles.description}>{description}</Text> : null}
 			{editing ? 
 				<TextInput 
@@ -100,7 +144,6 @@ const styles = StyleSheet.create({
 		backgroundColor: '#EFECF4',
 		alignItems: 'center',
 		
-		// justifyContent: 'center',
 	},
 	title: {
 		width: 375,
@@ -144,6 +187,53 @@ const styles = StyleSheet.create({
 		borderColor: "#384289",
 		color: "#384289",
 		paddingLeft: 15
+	},
+	buttonBS: {
+		width: 300,
+		height: 40,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: '#384289',
+		color: '#384289',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		// marginTop: 10,
+		marginBottom: 20,
+		paddingTop: 7,
+		alignItems: 'center'
+	},
+	headerBS: {
+		backgroundColor: '#fff',
+		shadowColor: '#333333',
+		shadowOffset: {width: -1, height: -3},
+		shadowRadius: 2,
+		shadowOpacity: 0.4,
+		paddingTop: 20,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20
+	},
+	panel: {
+		alignItems: 'center'
+	},
+	panelContent: {
+		backgroundColor: '#fff'
+	},
+	panelSubtitle: {
+		color: '#000',
+		fontSize: 18,
+		textAlign: 'center'
+	},
+	panelHandle: {
+		width: 40,
+		height: 8,
+		borderRadius: 4,
+		backgroundColor: '#00000040',
+		marginBottom: 10
+	},
+	uploadTitle: {
+		fontSize: 30,
+		color: '#384289',
+		marginBottom: 25
 	}
 });
 
